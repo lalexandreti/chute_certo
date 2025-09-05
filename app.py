56e4baf51c1fc0dece95b756c8187f37a2b05a40
Luiz Alexandre Rodrigues Silva <lalexandreti@users.noreply.huggingface.co>
2025-09-05 03:00:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import io
 import numpy as np
 import pandas as pd
-import matplotlib matplotlib.use("Agg")
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import gradio as gr
 

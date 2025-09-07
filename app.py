828cc34aab9337fa0ac501af01c57ed2e3ece526
Luiz Alexandre Rodrigues Silva <lalexandreti@users.noreply.huggingface.co>
2025-09-07 16:33:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -189,13 +189,13 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
                 seed_g = gr.Number(value=123, label="Semente preditiva")
 
             # 3) Saídas
-    gr.Markdown("Aqui está um resumo das informações fornecidas até o momento:)
+    gr.Markdown("Aqui está um resumo das informações fornecidas até o momento:")
             meta_out   = gr.Textbox(label="Resumo")
-    gr.Markdown("A partir do que foi rodada é possível levantar a seguiunte tabela de estatísticas:)
+    gr.Markdown("A partir do que foi rodada é possível levantar a seguiunte tabela de estatísticas:")
             table_out  = gr.Dataframe(label="Tabela com as Estatísticas", interactive=True)
-    gr.Markdown("Os resultados indicaram uma probabilidade média de conversão em torno de:)
+    gr.Markdown("Os resultados indicaram uma probabilidade média de conversão em torno de:")
             plot_post  = gr.Plot(label="Posterior de p")
-    gr.Markdown("A previsão de possibilidade de gols com 30 finalizações no alvo mostrou média de :)
+    gr.Markdown("A previsão de possibilidade de gols com 30 finalizações no alvo mostrou média de :")
             plot_pred  = gr.Plot(label="Preditiva de gols")
             
     
